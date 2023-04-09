a7ec2d7fc5f82a7f854d2e07659a2881012aeed1
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-04-09 23:49:52 +0530

    socket.io connection done

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(3000, () => {
+////////////////////////////////
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+////////////////////////////////
+
+app.use("/", express.static(__dirname + "/public"));
+
+server.listen(3000, () => {
   console.log("server listening on port 3000");
 });
